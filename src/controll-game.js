60bcf5aa31ba9f144ec34eda35b2844dd0d3abb3
Lais Frigério <laiss@gazetadopovo.com.br>
2020-06-28 12:03:19 -0300

    Refactor start game
    Signed-off-by: Lais Frigério <laiss@gazetadopovo.com.br>

--- src/controll-game.js
+++ src/controll-game.js
@@ -24,3 +24,18 @@ function pauseSound() {
     gameAudio.pause();
     play = false;
 }
+
+/**
+ * Init/Reset game
+ */
+function init() {
+    play = false;
+    scenario = new Scenario(scenarioImage, 3);
+    pointing = new Pointing();
+    witchCharacter = new Witch(witchCharacterMatrix, witchCharacterImage, 0, 30, 110, mainCharacterHeight, 220, 270);
+    bubbleEnemy = new Bubble(bubbleEnemyMatrix, bubbleEnemyImage, width - 52, 30, 52, 52, 104, 104, 10, 200);
+    flyingEnemy = new Bubble(flyingEnemyMatrix, flyingEnemyImage, width + width, 400, 52, 52, 200, 150, 10, 1500);
+    troll = new Troll(trollMatrix, trollImage, width + width, 0, 200, 200, 400, 400, 10, 2500);
+    enemies = [];
+    enemies.push(bubbleEnemy, flyingEnemy, troll);
+}
